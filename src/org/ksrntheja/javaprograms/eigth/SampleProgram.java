65f344d64adb9e81ed2261622945950d7184a9ee
KSRNTheja <srntkolla@gmail.com>
2019-12-12 21:51:52 +0530

    01-Added SampleProgram.

---
+++ src/org/ksrntheja/javaprograms/eigth/SampleProgram.java
@@ -0,0 +1,11 @@
+package org.ksrntheja.javaprograms.eigth;
+
+public class SampleProgram {
+
+    public static void main(String args[]) {
+
+        System.out.println("Sample Program");
+
+    }
+
+}
