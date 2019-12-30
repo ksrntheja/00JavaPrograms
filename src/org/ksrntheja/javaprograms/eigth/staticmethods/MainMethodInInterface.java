43d29fd9330db52360eec609bc268d18336afe15
KSRNTheja <srntkolla@gmail.com>
2019-12-30 12:20:48 +0530

    22-StaticMainMethodInInterface.

---
+++ src/org/ksrntheja/javaprograms/eigth/staticmethods/MainMethodInInterface.java
@@ -0,0 +1,13 @@
+package org.ksrntheja.javaprograms.eigth.staticmethods;
+
+interface MainMethodInInterface {
+
+    static void main(String args[]) {
+
+        System.out.println("MainMethodInInterface");
+
+    }
+
+}
+
+// MainMethodInInterface
