216a29a049126c4466eb00c102437a07cd09b269
KSRNTheja <srntkolla@gmail.com>
2020-01-03 11:05:11 +0530

    42-AutoBoxing.

---
+++ src/org/ksrntheja/javaprograms/eigth/primitivetypefunctionalinterfaces/AutoBoxingInteger.java
@@ -0,0 +1,17 @@
+package org.ksrntheja.javaprograms.eigth.primitivetypefunctionalinterfaces;
+
+public class AutoBoxingInteger {
+
+    public static void main(String[] args) {
+
+        Integer integer = 10;
+        // Integer integer = Integer.valueOf(10);
+        // Unnecessary boxing 'Integer.valueOf(10)'
+
+        System.out.println(integer);
+
+    }
+
+}
+
+// 10
